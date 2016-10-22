df27eac1e2a368d1bafcbb62111e5b97a755d0a1
Steven Burris <stevenburris.jr@gmail.com>
2016-10-22 14:16:17 -0400

    Uncommented some test methods, but didn't run the tests yet

--- tests/com/theironyard/MainTest.java
+++ tests/com/theironyard/MainTest.java
@@ -1,32 +1,33 @@
-//package com.theironyard;
-//
-//import org.junit.Test;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by stevenburris on 10/19/16.
-// */
-//public class MainTest {
-//    public Connection startConnection () throws SQLException {
-//        Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
-//        Main.createTables(conn);
-//        return conn;
-//    }
-//
-//    @Test
-//    public void testUser() throws SQLException {
-//        Connection conn = startConnection();
-//        Main.insertUser(conn, "Alice", "pass");
-//        User user = Main.selectUser(conn, "Alice");
-//        conn.close();
-//        assertTrue(user != null);
-//    }
+package com.theironyard;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stevenburris on 10/19/16.
+ */
+public class MainTest {
+    public Connection startConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
+        Main.createTables(conn);
+        return conn;
+    }
+
+    @Test
+    public void testUser() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertUser(conn, "Alice", "pass");
+        User user = Main.selectUser(conn, "Alice");
+        conn.close();
+        assertTrue(user != null);
+    }
+}
 //
 //    @Test
 //    public void testGalleries() throws SQLException {
